bf432c22151972905f2d2fe805a77840494c681a
Moaiad <38941803+moaiadN@users.noreply.github.com>
2023-06-10 22:02:10 +0300

    Delete static directory

--- static/js/script.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = function () {
-  if (window.pageYOffset > 200) {
-    document.querySelector(".arrowUp").style.display = "block";
-  } else {
-    document.querySelector(".arrowUp").style.display = "none";
-  }
-};
